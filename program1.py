3da24355fd14a43506af5bf9a5b080d0cff72366
Ayush Jha <ayushjha79aj@gmail.com>
2026-09-22 14:45:46 +0530

    initial commit

---
+++ program1.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+    order = []
+
+    while queue:
+        vertex = queue.popleft()
+        order.append(vertex)
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+        return order
+    
+def dfs(graph, start, visited = None, order = None):
+        if visited is None:
+            visited = set()
+        if order is None:
+            order = []
+
+        visited.add(start)
+        order.append(start)
+        for neighbor in graph[start]:
+            if neighbor not in visited:
+                dfs(graph, neighbor, visited, order)
+        return order
+    
+    #Example Usage
+if __name__ == "__main__":
+        graph = {
+            'A': ['B', 'C'],
+            'B': ['A', 'D', 'E'],
+            'C': ['A,', 'F'],
+            'D': ['B'],
+            'E': ['B', 'F'],
+            'F': ['C', 'E'],
+        }
+print("BFS traversal starting from 'A':", bfs(graph, 'A'))
+print("DFS traversal starting from 'A':", bfs(graph, 'A'))
